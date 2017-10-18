25c039a57d86f55eed7c171a3ed88664937a14c2
Andrei <reflection.dm@gmail.com>
2017-10-18 11:58:13 -0400

    Exclude extension and starting dot based on settings.

--- src/workspace-module-resolver.ts
+++ src/workspace-module-resolver.ts
@@ -1,6 +1,8 @@
-import { IWorkspaceModuleProvider } from './workspace-module-provider';
-import { matchByWords } from './utils';
 import * as path from 'path';
+
+import { IWorkspaceModuleProvider } from './workspace-module-provider';
+import { matchByWords, stripExtension } from './utils';
+import { getConfig } from './config';
 
 export function findWorkspaceModules(moduleProvider: IWorkspaceModuleProvider, currentDocumentPath: string, packageName: string) {
   if (!packageName) {
@@ -9,5 +11,20 @@ export function findWorkspaceModules(moduleProvider: IWorkspaceModuleProvider, c
 
   return moduleProvider.getWorkspaceModules()
     .filter((fileInfo) => matchByWords(packageName, fileInfo.fileName))
-    .map((fileInfo) => path.relative(currentDocumentPath, fileInfo.fsPath).replace('.', ''));
+    .map((fileInfo) => composeImportPath(currentDocumentPath, fileInfo.fsPath));
+}
+
+function composeImportPath(currentDocumentPath: string, fsPath: string) {
+  let relativePath = path.relative(currentDocumentPath, fsPath).replace('.', '');
+  const config = getConfig();
+
+  if (config.skipInitialDotForRelativePath && relativePath.startsWith('./..')) {
+    relativePath = relativePath.substring(2);
+  }
+
+  if (config.excludeExtension) {
+    relativePath = stripExtension(relativePath);
+  }
+
+  return relativePath;
 }
